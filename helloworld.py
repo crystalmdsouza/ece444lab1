10abc9bdcab6eb49eb5be36eb91a4ce626bb8868
Crystal D'Souza <crystalmariedsouza@Crystals-MacBook-Air.local>
2021-09-15 18:15:16 -0400

    Rebasing edits from rebase branch to develop

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,4 @@ print("Hello World! I have been at UofT for four years.")
 print("I have an instagram account for food reviews! It is called crystalscravings.")
 
 print("I coach first and second-year students on best practices for resumes and interviews.")
+print("My favourite colour is aqua!") 
