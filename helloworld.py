6d66b321e863acb86a9174a73ab1b26eb8fc9e31
Crystal D'Souza <crystalmariedsouza@Crystals-MacBook-Air.local>
2021-09-15 22:48:32 -0400

    Added second change to develop branch for exercise 5

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,5 @@
 print("Hello World! I have been at UofT for four years.")
  
-print("I have an instagram account for food reviews! It is called crystalscravings.") 
+print("I have an instagram account for food reviews! It is called crystalscravings.")
+
+print("I coach first and second-year students on best practices for resumes and interviews.")
